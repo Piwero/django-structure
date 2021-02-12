419bf433c5cf8b76db0b70928f5490f1681d6188
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-12 18:21:59 +0000

    add objects to services with photos

--- services/models.py
+++ services/models.py
@@ -7,7 +7,7 @@ class Service(models.Model):
 
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=400)
-    image = models.ImageField(upload_to="services/static/services")
+    image = models.ImageField(upload_to="services")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
