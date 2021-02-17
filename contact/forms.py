cd0bc831ead89e28fcec9871515c2d8d1b62bc6e
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-17 10:54:45 +0000

    add multiple choice

--- contact/forms.py
+++ contact/forms.py
@@ -5,7 +5,26 @@ from contact.models import Document
 class ContactForm(forms.Form):
     name = forms.CharField(label="Name", required=True, max_length=30)
     email = forms.EmailField(label="Email", required=True)
-    subject = forms.CharField(label="Subject", required=True, max_length=50)
+    CHOICES = (
+        ('--------', (
+            (1, '----------------------'),
+        )),
+        ('Error', (
+            (11, 'System Error'),
+            (12, 'Form Error'),
+            (13, 'Payment Error'),
+        )),
+        ('Questions', (
+            (21, 'Tutorial'),
+            (22, 'Payments'),
+        )),
+        ('Other', (
+            (31, 'Work with us'),
+            (32, 'Marketing'),
+            (33, 'Others')
+        )),
+    )
+    subject = forms.ChoiceField(choices=CHOICES)
     message = forms.CharField(label="Message", widget=forms.Textarea)
     upload = forms.FileField()
 
