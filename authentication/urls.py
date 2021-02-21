2a4ba3033a6f9f177a45a393a0562917b9c54177
Piwero <pinero@Joses-MacBook-Air.local>
2021-02-21 12:12:04 +0000

    create login required with func and class

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import signup
+from .views import signup, secret_page, SecretPage
 
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('accounts/', include("django.contrib.auth.urls")),
+    path('secret/', secret_page, name='secret'),
+    path('secret2/', SecretPage.as_view(), name='secret2'),
 
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+# for login required func
+from django.contrib.auth.decorators import login_required
+
+# for login required class
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+
 
 
 # Create your views here.
@@ -18,3 +28,12 @@ def signup(request):
     return render(request, "registration/signup.html", {
         "form": form
     })
+
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page2.html'
